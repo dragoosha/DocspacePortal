a18815798a9e237244cb31e57f65ab781d7bbc11
Vladzaharinsky <zahar.vlad23@gmail.com>
2025-02-06 17:45:39 +0300

    added splashScreen and logos

--- app/src/main/java/com/vzh/docspaceportal/presentation/screens/splashScreen/SplashScreen.kt
+++ app/src/main/java/com/vzh/docspaceportal/presentation/screens/splashScreen/SplashScreen.kt
@@ -1,13 +1,57 @@
 package com.vzh.docspaceportal.presentation.screens.splashScreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.vzh.docspaceportal.R
+import com.vzh.docspaceportal.presentation.common.navigation.AppRoute
+import com.vzh.docspaceportal.presentation.common.theme.DocspacePortalTheme
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(
     navController: NavController,
     token: String?
 ) {
+    LaunchedEffect(key1 = token) {
+        delay(1000)
+        if (token.isNullOrEmpty()) {
+            navController.navigate(AppRoute.Auth.route) {
+                popUpTo(AppRoute.Splash.route) { inclusive = true }
+            }
+        } else {
+            navController.navigate(AppRoute.Main.route) {
+                popUpTo(AppRoute.Splash.route) { inclusive = true }
+            }
+        }
+    }
 
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.logo_symbol),
+            contentDescription = null
+        )
+        
+        Spacer(modifier = Modifier.height(40.dp))
+        CircularProgressIndicator()
+    }
 }
